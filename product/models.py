63e9a34ad40e79ea2d102c3c3dcf483f7270959f
toshi <etoshi0228@gmail.com>
2019-11-07 18:50:38 +0900

    first

---
+++ product/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('カテゴリー名', max_length=32)
+    
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField('商品名', max_length=128)
+    price = models.PositiveIntegerField('価格')
